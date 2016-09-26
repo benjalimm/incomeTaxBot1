f662fc3eae7a9567a7197ce92477efbc6e776e5d
benjalimm <keymote@gmail.com>
2016-09-26 20:02:30 +0930

    hey

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
           sendTextMessage(sender, helloRandomizer)
-            text = event.message.text
+            text = event.message.text.toLowerCase()
             if (text === 'Generic') {
                 sendGenericMessage(sender)
                 continue
