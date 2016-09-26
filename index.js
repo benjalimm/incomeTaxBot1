5e4a468c1cc64d7aa99948a418caf0f6a1df90f0
benjalimm <keymote@gmail.com>
2016-09-26 13:43:25 +0930

    Hey

--- index.js
+++ index.js
@@ -98,8 +98,7 @@ function sendGenericMessage(sender) {
                 "elements": [{
                     "title": "First card",
                     "subtitle": "Element #1 of an hscroll",
-                    // "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "image_url": "https://www.kogan.com/au/buy/apple-iphone-7-256gb-black-apple/",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
                     "buttons": [{
                         "type": "web_url",
                         "url": "https://www.messenger.com",
