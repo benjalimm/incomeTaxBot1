fd19bccd98ee5863a5e26de5e18f2adfa6771ab1
benjalimm <keymote@gmail.com>
2016-11-29 13:21:07 +1030

    Incomebot

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
+var bot = require('./bot.js')
 //BotBuilder
 var builder = require('botbuilder');
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 //Bot Builder
-
-
 
 app.set('port', (process.env.PORT || 5000))
 
@@ -23,13 +22,14 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
+for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
+
 
 // Spin up the server
 app.listen(app.get('port'), function() {
@@ -47,24 +47,30 @@ app.post('/webhook/', function (req, res) {
         event = req.body.entry[0].messaging[i]
         sender = event.sender.id
         if (event.message && event.message.text) {
-          sendTextMessage(sender, helloRandomizer)
             text = event.message.text.toLowerCase()
-            if (text.contains("options")) {
-                sendGenericMessage(sender)
-                continue
-            } else if (text.contains("hey")) {
-              sendTextMessage(sender, "Hey there baby gurrlll")
-              break
+            if isNaN(text) == false {
+              income = parseInt(text) + "<br>";
+              sendTextMessage(sender, calcTax)
+              continue
+            } else {
+              let reply = "Your input has to be number. For example, if your annual income is $40000, you will just input 40000.";
+              sendTextMessage(sender, reply);
             }
-            // sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-            sendTextMessage(sender, "I'm not entirely sure what you're saying... Try me again")
-        }
-        if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-            continue
-        }
-    }
+        //     if (text.contains("options")) {
+        //         sendGenericMessage(sender)
+        //         continue
+        //     } else if (text.contains("hey")) {
+        //       sendTextMessage(sender, "Hey there baby gurrlll")
+        //       break
+        //     }
+        //     // sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        //     sendTextMessage(sender, "I'm not entirely sure what you're saying... Try me again")
+        // }
+        // if (event.postback) {
+        //     text = JSON.stringify(event.postback)
+        //     sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+        //     continue
+        // }
     res.sendStatus(200)
 })
 
